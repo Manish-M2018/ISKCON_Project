dbf62e4299378dfbcf1d0171ba9f56f8304c6c4a
Manish <42434096+Manish-M2018@users.noreply.github.com>
2019-05-20 09:45:36 +0530

    Add files via upload

--- customize_sadhana.js
+++ customize_sadhana.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
 
+  var japa_count = 0;
+  var hearing_count = 0;
+  var reading_count = 0;
+  var aarti_count = 0;
+
   $("#japa_form").hide();
   $("#hearing_form").hide();
   $("#reading_form").hide();
   $("#aarti_form").hide();
 
-  var japa_count = 0;
+  // setTimeout(function(){ alert("Hare Krishna , customize your sadhana by selecting the options and then entering the amount of Sadhana you have committed to."); }, 3000);
+
 
   $("#japa").click(function(){
+
     if(japa_count == 0)
     {
       $("#japa_form").show();
@@ -24,25 +31,56 @@ $(document).ready(function(){
         $("#japa_opt1").prop("checked",false);
         $("#japa_opt2").prop("checked",false);
       });
+
+      $("#japa_confirm_button").click(function(){
+        alert("Japa is confirm");
+      });
       japa_count = 1;
     }
     else {
-      count = 0;
+      japa_count = 0;
       $("#japa_form").hide();
     }
-
   });
 
   $("#hearing").click(function(){
-    $("#hearing_form").show();
+
+    if(hearing_count == 0)
+    {
+      $("#hearing_form").show();
+
+
+
+      hearing_count = 1;
+    }
+    else {
+      $("#hearing_form").hide();
+      hearing_count = 0;
+    }
   });
 
   $("#reading").click(function(){
-    $("#reading_form").show();
+    if(reading_count == 0)
+    {
+      $("#reading_form").show();
+      reading_count = 1;
+    }
+    else {
+      $("#reading_form").hide();
+      reading_count = 0;
+    }
   });
 
   $("#aarti").click(function(){
-    $("#aarti_form").show();
+    if(aarti_count == 0)
+    {
+      $("#aarti_form").show();
+      aarti_count = 1;
+    }
+    else {
+      $("#aarti_form").hide();
+      aarti_count = 0;
+    }
   });
 
 });
